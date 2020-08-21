2f4b545bf04a78fb93f0d157e9cb172a31a5c232
Dion Pettaway <dionp@fullsail.edu>
2020-08-21 12:01:13 -0400

    set initial state and reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useReducer} from 'react';
+import {API} from 'aws-amplify'
+import {List} from 'antd';
+import {listNotes} from './graphql/queries'
+
+import 'antd/dist/antd.css'
 import './App.css';
+
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: {name: '', description: ''}
+}
+
+function reducer(state, action) {
+  switch({type, notes}) {
+    case 'SET_NOTES':
+      return {...state, notes: notes, loading: false }
+    case 'ERROR':
+      return {...state, loading: false, error: true }
+    default:
+      return state
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
